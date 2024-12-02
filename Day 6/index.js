00bd4acf1ebe155f74e1bafe6f232d62413a7c86
iikdxaman <am19aman@gmail.com>
2024-12-02 15:44:05 +0530

    Day 6 Complete

---
+++ Day 6/index.js
@@ -0,0 +1,92 @@
+console.log("----Welcome----");
+
+
+document.write("Hello World");
+let a = 5;
+console.log(a)
+var b=5;
+console.log(b)
+const c=10;
+console.log(c);
+a=9;
+b=5;
+// c=1;
+
+
+
+
+
+
+
+// table print
+
+let n = 99;
+for (let i = 1; i <= 10; ++i)
+    console.log(n + " * " + i +" = " + n * i);
+
+
+// Print all numbers divisible by 3
+ 
+let N = 100; 
+   for (let i = 1; i <= n; i++) {
+        if (i % 3 == 0) {
+            console.log(i)
+        }
+    }
+
+    // Print all numbers divisible by 3 & 5.
+
+let A = 100;
+   for (let i = 1; i <= a; i++) {
+        if (i % 3 == 0 && i % 5 == 0) {
+            console.log(i)
+        }
+    }
+    
+// Print all numbers divisible by 3 & 5.
+
+let z = 100;
+   for (let i = 1; i <= z; i++) {
+        if (i % 3 == 0 && i % 5 == 0 && i % 9 == 0) {
+            console.log(i)
+        }
+    }
+
+// print even number and odd number
+
+let y = 100;
+    for (let i = 0; i <= y; i++) {
+        if (i % 2 == 0){
+            console.log("Even Number is :", i)
+        }
+        }
+
+
+// odd number
+
+let x = 100;
+    for (let i = 0; i <= x; i++) {
+        if (i % 2 !== 0){
+            console.log("Odd Number is :", i)
+        }
+        }
+
+
+// while loop
+let i = 0;
+while (i < 5) {
+    console.log(i);
+    i++
+}
+
+
+// reverse
+
+let v = 10;
+while (n >= 0) {
+    console.log(v);
+    v--;
+}
+
+
+
