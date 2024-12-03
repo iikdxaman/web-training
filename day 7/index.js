37d5fe9ec8abf17b10d9bc91258cc22968cccf34
iikdxaman <am19aman@gmail.com>
2024-12-03 15:55:51 +0530

    Add files via upload

---
+++ day 7/index.js
@@ -0,0 +1,41 @@
+console.log("-----!!Welcome Back!!------");
+
+const title = document.getElementById("title");
+console.log(title);
+
+title.innerText = "Hi I'm Coder";
+
+
+//const desc = document.getElementsByClassName("desc");
+//console.log(desc);
+
+//desc.innerText = "Hello";
+
+
+//divEle[2].innerText = "Third Div Tag"
+
+
+
+
+
+//for(let i = 0; i < divEle.lenght; i++) {
+//if (i % 2 === 0)
+// even index:
+//divEle[i].innerText = "Even";
+
+// odd index:
+//divEle[i].innerText = "odd";
+
+//}
+
+
+let n = ['1', '2', '3', '4'];
+
+
+for (let i = 0; i < n.length; i++) {
+    console.log(n[i]);
+}
+
+//print sum of all elements:
+//print sum of all odd elements:
+//print Elements : divisible by 3 and 5
